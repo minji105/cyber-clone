381a20ba35ed05d88ba106ae6edb1e3720035cf5
minji_105 <ajhjmj9@naver.com>
2025-05-20 17:24:41 +0900

    ✨ Feat: Header 컴포넌트 생성 (#2)
    - Header.jsx 파일 생성
    - 헤더 영역 마크업
    - App.js에 공통 컴포넌트로 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Router>
+        <Header />
+
+        <main>
+          <Routes>
+          </Routes>
+        </main>
+
+      </Router>
     </>
   );
 }
